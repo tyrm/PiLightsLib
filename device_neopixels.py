f03aba23f0876629fed75705d566d4bdbbc2399f
Tyr <tyr@discowolf.local>
2016-12-05 21:22:12 -0800

    added GRB mode options

--- device_neopixels.py
+++ device_neopixels.py
@@ -7,27 +7,29 @@ from lights import DeviceObj
 
 # UnicornHat Object
 class NeoPixels(DeviceObj):
-    def __init__(self,  name, count):
+    def __init__(self, name, count, bri=255, grb=False):
         DeviceObj.__init__(self, name, "neopixel", count)
 
         # LED strip configuration:
-        LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
-        LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-        LED_DMA = 5  # DMA channel to use for generating signal (try 5)
-        LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-        LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+        pin = 18  # GPIO pin connected to the pixels (must support PWM!).
+        freq_hz = 800000  # LED signal frequency in hertz (usually 800khz)
+        dma = 5  # DMA channel to use for generating signal (try 5)
+        invert = False  # True to invert the signal (when using NPN transistor level shift)
 
         # Create NeoPixel object with appropriate configuration.
-        self.strip = Adafruit_NeoPixel(count, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        self.grb = grb
+        self.strip = Adafruit_NeoPixel(count, pin, freq_hz, dma, invert, bri)
 
         self.strip.begin()
-
 
     def set(self, r, g, b, x=0, y=0):
         DeviceObj.set(self, r, g, b, x, y)
 
         # Set Pixel
-        self.strip.setPixelColor(x, Color(r, g, b))
+        if not self.grb:
+            self.strip.setPixelColor(x, Color(r, g, b))
+        else:
+            self.strip.setPixelColor(x, Color(g, r, b))
 
     def show(self):
         DeviceObj.show(self)
